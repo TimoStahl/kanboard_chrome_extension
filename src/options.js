5879b765226ce0bc9dc4742d6333cbffeb8b77dd
BlueTeck <tili2@gmx.de>
2015-02-04 19:20:47 +0100

    multilanguage

--- src/options.js
+++ src/options.js
@@ -1,29 +1,30 @@
 function add_url() {
-	var url = $('#url').val();	
-	var token = $('#token').val();	
+	var url = $('#url').val();
+	var token = $('#token').val();
 	var api = url + ';' + token;
-	var name = 'Testname';
-	
+	var name = $('#name').val();
+
 	var api_new = {url:url, token:token, name:name};
-	
+
 	//ToDo: validate URL
 	if(url.length > 20 && token.length > 20){
-		
+
 		//reset fields
 		$('#token').val('');
 		$('#url').val('');
-		
+		$('#name').val('');
+
 			//get endpoints from storage:
 			chrome.storage.sync.get('endpoints', function(items) {
 				if(typeof items.endpoints === 'undefined'){
 					var endpoints = [api_new];
 				}else{
 					var endpoints = items.endpoints;
-					endpoints.push(api_new);					
-				}	
+					endpoints.push(api_new);
+				}
 				chrome.storage.sync.set({endpoints: endpoints}, function() {
 					addApiToTable(api_new);
-				}); 				
+				});
 			});
 	}
 }
@@ -36,7 +37,7 @@ function addApiToTable(endpoint) {
 function restore_options() {
 	var endpoints = [];
 	chrome.storage.sync.get( 'endpoints', function(items) {
-		items.endpoints.forEach(function(entry) {						
+		items.endpoints.forEach(function(entry) {
 			addApiToTable(entry);
 		});
 	});
@@ -44,9 +45,19 @@ function restore_options() {
 }
 
 $( document ).ready( function() {
+
+  	  //languages in html
+	  $('._locale_name').text(chrome.i18n.getMessage("name"));
+	  $('._locale_api_url').text(chrome.i18n.getMessage("api_url"));
+	  $('._locale_token').text(chrome.i18n.getMessage("token"));
+	  $('._locale_settings').text(chrome.i18n.getMessage("settings"));
+	  $('._locale_reset').text(chrome.i18n.getMessage("reset"));
+	  $('._locale_add').text(chrome.i18n.getMessage("add"));
+	  $('._locale_endpoints').text(chrome.i18n.getMessage("endpoints"));
+
 	restore_options();
 	$('#add').click( function() {
-		add_url(); 
+		add_url();
 	});
 	$('#reset').click( function() {
 		chrome.storage.sync.clear(function (){
--- src/popup.js
+++ src/popup.js
@@ -1,11 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
 	$(function() {
+	  //languages in html
+	  $('._locale_projects').text(chrome.i18n.getMessage("projects"));
+
 		//enable a href links and open in a new tab
 		$('body').on('click', 'a', function(){
 			chrome.tabs.create({url: $(this).attr('href')});
 			return false;
 		});
-	  
+
 		//Test hover icon (did not work)
 		$( ".display" ).on( "mouseenter", function() {
 			$(this).addClass('fa fa-eye');
@@ -15,48 +18,54 @@ document.addEventListener('DOMContentLoaded', function () {
 			$(this).removeClass('fa fa-eye');
 			console.log('leave display');
 		});
-	
-		//get apis from local storage settings		
+
+		//get apis from local storage settings
 		chrome.storage.sync.get( 'endpoints', function(items) {
-			items.endpoints.forEach(function(endpoint) {				
-				
-				//prepare api call
-				var user = 'jsonrpc';
-				var request = '{"jsonrpc": "2.0", "method": "getAllProjects", "id": 1}';
-				var url = endpoint.url.substring(0, endpoint.url.length - 11);
-		
-				//add new api endpoint to list
-				$("#projects").append('<br><li>' + endpoint.name + '<ul data-url="' + url + '"></ul></li>');
-				
-				//make api call
-				$.ajax({
-					url: endpoint.url,
-					type: 'POST',
-					dataType: 'json',
-					crossDomain: true, 
-					data: request,
-					beforeSend: function(xhr){
-						var auth = Base64.encode(user + ':' + endpoint.token); 
-						xhr.setRequestHeader("Authorization", "Basic " + auth);
-					},
-					success: function(data) {
-						var result = data.result;
-						//loop to result (projectlist)
-						$.each(result, function(index, element) {		
-				
-							var display = url + '?controller=board&action=show&project_id=' + element.id ;
-							var add = url + '?controller=task&action=create&project_id=' + element.id ;
-							var row = '<li><a class="display" href="' + display + '">' + element.name + '</a> <a class="add fa fa-plus" href="' + add + '"> add Task </a></li>';
-						
-							$("ul").find("[data-url='" + url + "']").append(row);
-						});			
-					},
-					error: function(data){
-						$("ul").find("[data-url='" + url + "']").append('<li>Error due API call.</li>');
-					}
-				});
-			});
+		  if(typeof items.endpoints === 'undefined'){
+		    //TODO change extension ID
+			  $("#projects").append('<li>' + chrome.i18n.getMessage("no_api_endpoint_found") + '<a href="chrome://extensions?options=dpijbbnoajflcejcdngjcbacedpchceg"> ' + chrome.i18n.getMessage("go_to_options") + ' </a></li>');
+			}else{
+
+  			items.endpoints.forEach(function(endpoint) {
+
+  				//prepare api call
+  				var user = 'jsonrpc';
+  				var request = '{"jsonrpc": "2.0", "method": "getAllProjects", "id": 1}';
+  				var url = endpoint.url.substring(0, endpoint.url.length - 11);
+
+  				//add new api endpoint to list
+  				$("#projects").append('<br><li>' + endpoint.name + '<ul data-url="' + url + '"></ul></li>');
+
+  				//make api call
+  				$.ajax({
+  					url: endpoint.url,
+  					type: 'POST',
+  					dataType: 'json',
+  					crossDomain: true,
+  					data: request,
+  					beforeSend: function(xhr){
+  						var auth = Base64.encode(user + ':' + endpoint.token);
+  						xhr.setRequestHeader("Authorization", "Basic " + auth);
+  					},
+  					success: function(data) {
+  						var result = data.result;
+  						//loop to result (projectlist)
+  						$.each(result, function(index, element) {
+
+  							var display = url + '?controller=board&action=show&project_id=' + element.id ;
+  							var add = url + '?controller=task&action=create&project_id=' + element.id ;
+  							var row = '<li><a class="display" href="' + display + '">' + element.name + '</a> <a class="add fa fa-plus" href="' + add + '"> ' + chrome.i18n.getMessage("add_task") + ' </a></li>';
+
+  							$("ul").find("[data-url='" + url + "']").append(row);
+  						});
+  					},
+  					error: function(data){
+  						$("ul").find("[data-url='" + url + "']").append('<li>' + chrome.i18n.getMessage("error_due_api_call") + '</li>');
+  					}
+  				});
+  			});
+			}
 		});
-	});  
+	});
 });
 
